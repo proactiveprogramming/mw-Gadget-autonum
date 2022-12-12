e5b755364d60cea0b1983a9f9ef5cd517e22f09a
Melissa Janine Newman <51647658+proactiveprogramming@users.noreply.github.com>
2022-12-12 12:56:56 +0200

    Version 1.1 Proactive Programming

--- Gadget-autonum.js
+++ Gadget-autonum.js
@@ -9,7 +9,7 @@ var toc = document.querySelector('#toc');
 if (toc) {
   document.querySelectorAll('.mw-parser-output :is(h1,h2,h3,h4,h5,h6) .mw-headline').forEach(function (headline) {
     var num = toc.querySelector('a[href="#' + CSS.escape(headline.id) + '"] .tocnumber');
-    if (num) headline.prepend(num.textContent + ' ');
+    if (num) headline.prepend(num.textContent + '. ');
   });
 } else {
   document.body.classList.add('tpl-autonum');
